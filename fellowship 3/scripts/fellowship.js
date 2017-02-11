cba8924dfa2c27df15e286e0ab693c64a5335609
John Favors <JohnnyFavorite@Johns-MacBook-Pro.local>
2017-02-11 16:10:46 -0600

    part 1 working

--- fellowship 3/scripts/fellowship.js
+++ fellowship 3/scripts/fellowship.js
@@ -19,5 +19,43 @@ var buddies = [
 var lands = ['The Shire', 'Rivendell', 'Mordor'];
 var body = document.body;
 
+//Part 1
+var makeMiddleEarth = function () {
+   // create a section tag with an id of `middle-earth`
+   var middle_earth = document.createElement("section");
+   // add each land as an `article` tag
+  for (var landNames in lands){
+  	var article = document.createElement('article');
+  	console.log(article);
+  } 
+   // inside each `article` tag include an `h1` with the name of the land
+  for (var h1LandNames in landNames){
+  	var landh1 = document.createElement('h1');
+    console.log(landh1);
+  	landh1.innerHHTML = lands[landNames];
+  }
+   // append `middle-earth` to your document `body`
+   body.appendChild(middle_earth);
 
+};
+
+makeMiddleEarth();
+
+//Part 2
+var makeHobbits = function () {
+   // display an `unordered list` of hobbits in the shire
+   // (which is the second article tag on the page)
+   var ulhobbits = document.createElement('ul');
+   
+   for (var hobbitNames in hobbits){
+   	var lihobbits = document.createElement('li');
+   	console.log(li);
+   }
+  // give each hobbit a class of `hobbit` 
+    for (var hobbitClass in hobbitNames){
+      var clHobbits = document.creatElement.class('hobbit');
+      console.log(clHobbits);
+    }
+  
+};
 

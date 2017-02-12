e781c5b76dd8aa4bc57c7630b8d905087686dd3f
“JASON” <jrcorey84@gmail.com>
2017-02-12 17:09:49 -0600

    overdue progress commit. At part 11

--- fellowship 3/scripts/fellowship.js
+++ fellowship 3/scripts/fellowship.js
@@ -19,7 +19,7 @@ var buddies = [
 var lands = ['The Shire', 'Rivendell', 'Mordor'];
 var body = document.body;
 
-//Making Middel Earth
+// Making Middel Earth
 var section = document.createElement('section');
 section.id = 'middle-earth';
 
@@ -30,9 +30,10 @@ for(var i=0; i < lands.length; i++){
 	var names = document.createElement('h1');
 	names.innerHTML=lands[i];
 	article.appendChild(names);
-}
+};
 body.appendChild(section);
-}
+};
+
 makeMiddelEarth();
 
 // Making the Hobbits
@@ -49,9 +50,10 @@ for(var i=0; i < hobbits.length; i++){
 	};
 	section.children[0].appendChild(asideOne);
 };
+
 makeHobbits();
 
-//Making the Ring
+// Making the Ring
 var theRing = document.createElement('div');
 	theRing.id = 'the-ring';
 	theRing.className='magic-imbued-jewelry';
@@ -62,7 +64,7 @@ var keepItSecretKeepItSafe = function(){
 }
 keepItSecretKeepItSafe();
 
-//Make the Buddies
+// Make the Buddies
 var asideTwo = document.createElement('aside');
 var buddyList = document.createElement('ul');
 asideTwo.appendChild(buddyList);
@@ -78,23 +80,107 @@ var makeBuddies = function(){
 
 makeBuddies();
 
-//Change 'Strider' text to 'Aragorn'
+// Change 'Strider' text to 'Aragorn'
 var beautifulStranger = function(){
-asideTwo.children[0].children[3].innerHTML='Aragorn'
+	asideTwo.children[0].children[3].innerHTML='Aragorn'
 };
-
 beautifulStranger();
 
-//Move Hobbits to Rivendell
-
+// Move Hobbits to Rivendell
 var leaveTheShire = function(){
-	var movingHobbits = asideOne.children[0];
+	var movingHobbits = asideOne;
 	section.children[1].appendChild(movingHobbits);
 };
 
 leaveTheShire();
 
-//Forge the Fellowship
+// Forge the Fellowship
+var theFellowship = document.createElement('div')
+section.children[1].appendChild(theFellowship);
+var forgeTheFellowship = function(){
+	var movingHobbits = section.children[1].children[2].children[0].children;
+	var movingBuddies = section.children[1].children[1].children[0].children;
+
+	// for(var i=0; i < movingHobbits.length; i++){
+	// 	alert(movingHobbits[i].innerText + ' has joined the Fellowship')
+	// }
+	// for(var i = 0; i < movingBuddies.length; i++){
+	// 	alert(movingBuddies[i].innerText + ' has joined the Fellowship')
+	// }
+	
+	var fellowshipDiv = section.children[1].children[3];
+
+	var movedBuddies = section.children[1].children[1];
+	fellowshipDiv.appendChild(movedBuddies)
+	var movedHobbits = section.children[1].children[1];
+	fellowshipDiv.appendChild(movedHobbits)
+}
+
+forgeTheFellowship();
+
+// The Balrog
+var theBalrog = function(){
+	var gandalf = document.getElementsByTagName('li')[0];
+	gandalf.innerText = 'Gandalf the White';
+	gandalf.setAttribute('class', 'gandalf');
+
+}
+
+theBalrog();
+
+//	Horn of Gondor
+var hornOfGondor = function(){
+	// alert("The Horn of Gondor has been blown. Boromir's been killed by the Uruk-hai!")
+	var boromir = document.getElementsByTagName('li')[4];
+	boromir.remove();
+}
+
+hornOfGondor();
+
+// It's dangerous to go alone
+var mordor = document.getElementsByTagName('article')[2];
+var itsDangerousToGoAlone = function(){
+	var frodo = document.getElementsByTagName('li')[4];
+		mordor.appendChild(frodo);
+	var sam = document.getElementsByTagName('li')[4];
+		mordor.appendChild(sam)
+	var mountDoom = document.getElementsByTagName('article')[2];
+	var doom = document.createElement('div');
+		mountDoom.appendChild(doom);
+	doom.id = 'mount-doom';
+}
+itsDangerousToGoAlone();
+
+// We wants it
+var weWantsIt = function(){
+	var gollum = document.createElement('div');
+		gollum.id = 'gollum';
+	mordor.appendChild(gollum);
+
+	var theRing = document.getElementsByTagName('div')[2];
+	gollum.appendChild(theRing);
+
+	var toMountDoom = document.getElementsByTagName('div')[2];
+	toMountDoom.appendChild(gollum);
+};
+weWantsIt();
+
+// There and back again
+
+var thereAndBackAgain = function(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
